65f779107fc7aff1fa2547039568c634d4f5c000
legends-killer <958775729@qq.com>
2020-01-26 16:15:56 +0800

    Initial Commit

---
+++ ScheduleManager/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  ScheduleManager
+//
+//  Created by ljz on 2020/1/26.
+//  Copyright © 2020 DevHexgram. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScheduleManagerTests/ScheduleManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  ScheduleManagerTests.swift
+//  ScheduleManagerTests
+//
+//  Created by ljz on 2020/1/26.
+//  Copyright © 2020 DevHexgram. All rights reserved.
+//
+
+import XCTest
+@testable import ScheduleManager
+
+class ScheduleManagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
