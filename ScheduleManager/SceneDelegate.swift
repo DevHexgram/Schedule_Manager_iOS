e1f39e327dddfdb7b03d78c0d6b44205fc3863e4
legends-killer <958775729@qq.com>
2020-01-30 20:08:35 +0800

    initial commit

--- ScheduleManager/SceneDelegate.swift
+++ ScheduleManager/SceneDelegate.swift
@@ -21,11 +21,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
-
+        let loginView = LoginView()
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
+            window.rootViewController = UIHostingController(rootView: loginView)
             self.window = window
             window.makeKeyAndVisible()
         }
