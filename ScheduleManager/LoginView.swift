c13ca01ecfccd73d4dffecc430b8afb61d5c4af0
legends-killer <958775729@qq.com>
2020-01-30 20:08:22 +0800

    initial commit

---
+++ ScheduleManager/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  ScheduleManager
+//
+//  Created by ljz on 2020/1/26.
+//  Copyright © 2020 DevHexgram. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
