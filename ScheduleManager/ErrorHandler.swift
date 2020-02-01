4b0950484168699f1fade5db0e420b79cc5e152e
legends-killer <958775729@qq.com>
2020-02-02 02:24:31 +0800

    add: error code handler

---
+++ ScheduleManager/ErrorHandler.swift
@@ -0,0 +1,21 @@
+//
+//  errorHandler.swift
+//  ScheduleManager
+//
+//  Created by ljz on 2020/2/1.
+//  Copyright © 2020 DevHexgram. All rights reserved.
+//
+
+import SwiftUI
+
+struct errorHandler: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct errorHandler_Previews: PreviewProvider {
+    static var previews: some View {
+        errorHandler()
+    }
+}
